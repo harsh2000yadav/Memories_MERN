389664ccb0c6cd580c8bc1e2698f8acbba650ae8
harsh2000yadav <harsh2000yadav@gmail.com>
2021-01-19 13:21:17 +0530

    Mongo

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://yharsh:yharsh123@cluster0.gb7dq.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message))
+
+mongoose.set('useFindAndModify', false)
