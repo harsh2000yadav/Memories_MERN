d3011e720f8d5e7a685afd3b8d1573cf6c38ce58
harsh2000yadav <harsh2000yadav@gmail.com>
2021-01-20 13:16:56 +0530

    Restructure and comps

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Container, Grid, Typography, AppBar, Grow} from '@material-ui/core'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 
 const App = () =>{
     return(
         <div>
-            <h1>App</h1>
+            <Container maxWidth='lg'>
+                <AppBar position='static' color='inherit'>
+                    <Typography variant='h2' align='center'>Memories</Typography>
+                </AppBar>
+
+                <Grow in>
+                    <Container>
+                        <Grid container justify='space-between' alignItems='stretch' spacing = {3}>
+                            <Grid item xs={12} sm={7}>
+                                <Posts />
+                            </Grid>
+
+                            <Grid item xs={12} sm={4}>
+                                <Form />
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Grow>
+            </Container>
         </div>
     )
 }
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <h2>FORM</h2>
+        </div>
+    )
+}
+
+export default Form
