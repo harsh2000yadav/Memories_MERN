2c6c608b80de4cc20d3c2568e24220ef0735588f
harsh2000yadav <harsh2000yadav@gmail.com>
2021-01-21 11:29:19 +0530

    Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStrore, applyMiddleware, compose } from 'redux'
+import { thunk } from 'redux-thunk'
+import reducers from './reducers'
 
 import App from './App'
+
+const store = createStrore(reducers,compose(applyMiddleware(thunk)));
 
 ReactDom.render(<App />, document.getElementById('root'))
