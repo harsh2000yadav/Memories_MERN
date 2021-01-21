cf13a2742da8157bd9df33a25fd04c361ae9fdc3
harsh2000yadav <harsh2000yadav@gmail.com>
2021-01-21 11:11:19 +0530

    CSS

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { Container, Grid, Typography, AppBar, Grow} from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles'
 
 const App = () =>{
+    const classes = useStyles();
     return(
         <div>
             <Container maxWidth='lg'>
-                <AppBar position='static' color='inherit'>
-                    <Typography variant='h2' align='center'>Memories</Typography>
+                <AppBar className={classes.appBar} position='static' color='inherit'>
+                    <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
                 </AppBar>
 
                 <Grow in>
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Post from './Post/Post'
+import useStyles from './Styles'
 
 const Posts = () => {
+    const classes = useStyles();
     return (
         <div>
             <h2>Posts</h2>
